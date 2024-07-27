ae9c1632c00307301ade3cb163ffbbd00faaa3cd
dinh <dinhtq170601@gmail.com>
2024-07-27 23:06:30 +0700

    config gateway grpc microservice proto

--- apps/auth/src/main.ts
+++ apps/auth/src/main.ts
@@ -2,20 +2,21 @@ import { NestFactory } from '@nestjs/core';
 import { AuthModule } from './auth.module';
 import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 import { join } from 'path';
-import { AUTH_PACKAGE_NAME } from '@app/common';
+import { AUTH_PACKAGE_NAME, MyValidationPipe } from '@app/common';
 
 async function bootstrap() {
-  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
-    AuthModule,
-    {
-      transport: Transport.GRPC,
-      options: {
-        protoPath: join(__dirname, '../auth.proto'),
-        package: AUTH_PACKAGE_NAME,
-      },
-    },
-  );
+  const app = await NestFactory.create(AuthModule);
 
-  await app.listen();
+  app.useGlobalPipes(new MyValidationPipe());
+
+  app.connectMicroservice<MicroserviceOptions>({
+    transport: Transport.GRPC,
+    options: {
+      protoPath: join(__dirname, '../auth.proto'),
+      package: AUTH_PACKAGE_NAME,
+    },
+  });
+
+  await app.startAllMicroservices();
 }
 bootstrap();
